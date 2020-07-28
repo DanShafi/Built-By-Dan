6e8dc348a8456714b10bb7d64579b8a21e40c243
Danish Shafi <danish.shafi@live.co.uk>
2020-07-28 22:50:22 +0100

    Typical library added to project

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,33 @@
 import React from "react"
+import Typical from "react-typical"
 
-const Home = () => {
-    return (
-        <div className="container p-0">
-            <div>
-                <h1 className="event dark-hero-text">Frontend developer and web designer.</h1>
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="container p-0">
+                <div>                
+                    <Typical 
+                    className ="dark-hero-text"
+                    wrapper="h1"
+                    steps={["Frontend developer and web designer "]}
+                />
+                </div>
+
+                <div className ="hr-line" />
+
+                <div className="row sub-text">
+                    <h5 style={{fontWeight: 600}} className="col-lg-6">Hi, i'm Dan (short for Danish, pronounced \ ˈdan-ish)</h5>
+                    <div className="w-100"/>
+                    <p className="col-lg-6">
+                    A self-taught front end developer and web designer who escaped the 
+                    wonderful world of advertising analytics after a five-year(ish) stint. 
+                    An aficiando for simple, beautiful UI and the code to match. 
+                    There is no smoke and mirrors here - WYSIWYG.
+                    </p>
+                </div>
             </div>
-
-            <div className ="hr-line" />
-
-            <div className="row sub-text">
-                <h5 style={{fontWeight: 600}} className="col-lg-6">Hi, i'm Dan (short for Danish, pronounced \ ˈdan-ish)</h5>
-                <div className="w-100"/>
-                <p className="col-lg-6">
-                A self-taught front end developer and web designer who escaped the 
-                wonderful world of advertising analytics after a five-year(ish) stint. 
-                An aficiando for simple, beautiful UI and the code to match. 
-                There is no smoke and mirrors here - WYSIWYG.
-                </p>
-            </div>
-        </div>
-    ) 
+        ) 
+    }
 }
 
 export default Home
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,7 @@ class Navigation extends React.Component {
             <Router>
                 <Container>
                     <Navbar style={{fontSize: 15}} expand="lg" className="nav-gap navbar-dark nav-style" >
-                        <Navbar.Brand href="/" style={{fontSize: 15, fontWeight: "bold"}}>Built By Dan.</Navbar.Brand>
+                        <Navbar.Brand href="/" style={{fontSize: 15, fontWeight: "bold"}}> <img src="https://images.vexels.com/media/users/3/143495/isolated/preview/6b80b9965b1ec4d47c31d7eccf8ce4b0-yellow-lightning-bolt-icon-by-vexels.png" height="20px"/> Built By Dan.</Navbar.Brand>
 
                         <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
 
